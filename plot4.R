3747e5899531ddca9b723c869320b406eb5af9da
mfaerman <mfaerman@gmail.com>
2015-08-10 16:57:17 -0400

    Success with Plot4

--- plot4.R
+++ plot4.R
@@ -40,19 +40,31 @@ end <- as.POSIXct("2007-02-03")
 sel <- data[(data$pdate >= start) & (data$pdate < end)]
 sel$Global_active_power <- as.numeric(sel$Global_active_power)
 
-label <- "Global Active Power (kilowatts)"
-par("bg" = "transparent")
-par("pty" = "s")          ## "s" generates a square plotting region
-par("cex" = 0.75)         ## scale text size
+## Plot
+.pardefault <- par(no.readonly = TRUE)
 
-## Plot 3
-with(sel, plot(pdate, Sub_metering_1, type = "s", col="black", 
+png(file = "plot4.png", width = 480, height = 480)
+
+par(mfcol=c(2,2))
+
+## SubPlot Global Active Power
+label <- "Global Active Power"
+with(sel, plot(pdate, Global_active_power, type = 's', ylab = label, xlab = ""))
+
+## SubPlot sub metering
+with(sel, plot.default(pdate, Sub_metering_1, type = "s", col="black", 
                ylab = "Energy sub metering", xlab =""))
 with(sel, points(pdate, Sub_metering_2, type = "s", col="red"))
 with(sel, points(pdate, Sub_metering_3, type = "s", col="blue"))
-legend("topright", lty = rep(1,3), col = c("black", "red", "blue"), 
+legend("topright", lty = rep(1,3), col = c("black", "red", "blue"), bty = "n",
        legend = names(sel)[7:9])
 
+## SubPlot Voltage
+label <- "Global Active Power"
+with(sel, plot(pdate, Voltage, type = 's', ylab = "Voltage", xlab = "datetime"))
 
-dev.copy(png, file = "plot4.png", width = 480, height = 480)
+## SubPlot Global Reactive Power
+with(sel, plot(pdate, Global_reactive_power, type = 's', xlab = "datetime"))
+
 dev.off()
+par(.pardefault)
