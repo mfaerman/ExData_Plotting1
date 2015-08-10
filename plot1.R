128cd7116786a7a2edc50a785e7b096533b6f052
mfaerman <mfaerman@gmail.com>
2015-08-10 17:11:50 -0400

    Added corrections after Plot4 implementation.

--- plot1.R
+++ plot1.R
@@ -1,40 +1,53 @@
-library(data.table)
+require(data.table)
 
 data_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
-temp <- tempfile()
-download.file(data_url, temp, method = "curl")
-unzip(temp)
-unlink(temp)
+retrieve_data <- function() {
+        temp <- tempfile()
+        download.file(data_url, temp, method = "curl")
+        unzip(temp)
+        unlink(temp)        
+}
+
+## retrieve_data()
 
 data_file <- "household_power_consumption.txt"
 
-headers <- fread(data_file, sep = ";", nrow = 0)
+system.time(headers <- fread(data_file, sep = ";", nrow = 0))
 
-## Used to amortize time and memory usage 
+# The "skip" and "nrows" parameters are used to amortize 
+# time and memory usage when loading the data from the input 
+# file with the "fread()" function.
+
 ## Estimated lines to skip from input file (no needed dates)
 skip <- 65000 
 ## Estimated lines to read from input file
 nrows <- 5000
 
-data <- fread(data_file, sep = ";", na.strings = "?", colClasses = "character", 
-              skip = skip, nrows = nrows)
-unlink(data_file)
+system.time(data <- fread(data_file, sep = ";", na.strings = "?", colClasses = "character", 
+                          skip = skip, nrows = nrows))
+# unlink(data_file)
 
 setnames(data, names(headers))
 
-data$Date <- as.Date(strptime(data$Date, "%d/%m/%Y"))
+data[ , pdate := as.POSIXct(strptime(paste(data$Date, data$Time), 
+                                     "%d/%m/%Y %H:%M:%S"))]
+setkey(data, pdate)
 
-sel <- data[(data$Date == "2007-02-01") | (data$Date == "2007-02-02")]
+start <- as.POSIXct("2007-02-01")
+end <- as.POSIXct("2007-02-03")
 
-title <- "Global Active Power"
-label <- "Global Active Power (kilowatts)"
-
+sel <- data[(data$pdate >= start) & (data$pdate < end)]
 sel$Global_active_power <- as.numeric(sel$Global_active_power)
 
+## Plot
+.pardefault <- par(no.readonly = TRUE)
 
-par("bg" = "transparent")
+png (file = "plot1.png", width = 480, height = 480)
+
+title <- "Global Active Power"
+label <- paste(title, "(kilowatts)")
 hist(sel$Global_active_power, col = "red", main = title, xlab = label)
 
-dev.copy(png, file = "plot1.png", width = 480, height = 480)
 dev.off()
+par(.pardefault)
