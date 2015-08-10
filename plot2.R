9198b3565018873e7a0e70038c9daae904d7d722
mfaerman <mfaerman@gmail.com>
2015-08-10 17:19:04 -0400

    Plot2 corrections, based on Plot4 code.

--- plot2.R
+++ plot2.R
@@ -1,45 +1,52 @@
-library(data.table)
+require(data.table)
 
 data_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
-temp <- tempfile()
-download.file(data_url, temp, method = "curl")
-unzip(temp)
-unlink(temp)
+retrieve_data <- function() {
+        temp <- tempfile()
+        download.file(data_url, temp, method = "curl")
+        unzip(temp)
+        unlink(temp)        
+}
+
+## retrieve_data()
 
 data_file <- "household_power_consumption.txt"
 
-headers <- fread(data_file, sep = ";", nrow = 0)
+system.time(headers <- fread(data_file, sep = ";", nrow = 0))
 
-## Used to amortize time and memory usage 
+# The "skip" and "nrows" parameters are used to amortize 
+# time and memory usage when loading the data from the input 
+# file with the "fread()" function.
+
 ## Estimated lines to skip from input file (no needed dates)
 skip <- 65000 
 ## Estimated lines to read from input file
 nrows <- 5000
 
-data <- fread(data_file, sep = ";", na.strings = "?", colClasses = "character", 
-              skip = skip, nrows = nrows)
-unlink(data_file)
+system.time(data <- fread(data_file, sep = ";", na.strings = "?", colClasses = "character", 
+                          skip = skip, nrows = nrows))
+# unlink(data_file)
 
 setnames(data, names(headers))
 
-##data$Date <- as.Date(strptime(data$Date, "%d/%m/%Y"))
-d <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
-d <- as.POSIXct(d)
+data[ , pdate := as.POSIXct(strptime(paste(data$Date, data$Time), 
+                                     "%d/%m/%Y %H:%M:%S"))]
+setkey(data, pdate)
 
-start <- as.POSIXct("2007-02-01 00:00:00")
-end <- as.POSIXct("2007-02-02 23:59:59")
-dsel <- d[(d >= start) & (d <= end)]
-sel <- data[(d >= start) & (d <= end)]
+start <- as.POSIXct("2007-02-01")
+end <- as.POSIXct("2007-02-03")
 
-
-label <- "Global Active Power (kilowatts)"
-
+sel <- data[(data$pdate >= start) & (data$pdate < end)]
 sel$Global_active_power <- as.numeric(sel$Global_active_power)
 
+## Plot
+.pardefault <- par(no.readonly = TRUE)
 
-par("bg" = "transparent")
-plot(dsel, sel$Global_active_power, type = 's', ylab = label, xlab = "")
+png(file = "plot2.png", width = 480, height = 480)
+title <- "Global Active Power"
+label <- paste(title, "(kilowatts)")
+with (sel, plot(pdate, Global_active_power, type = 's', ylab = label, xlab = ""))
 
-dev.copy(png, file = "plot2.png", width = 480, height = 480)
 dev.off()
+par(.pardefault)
